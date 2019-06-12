6ddbd4f2e3768d9f34d5dd9c6abdd750f31d5196
keets zhu <928420597@qq.com>
2019-06-12 08:31:15 +0800

    ch6

--- ch6-config/resume.go
+++ ch6-config/resume.go
@@ -3,6 +3,7 @@ package main
 import (
 	"ch6-config/conf"
 	"fmt"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 )
@@ -11,7 +12,7 @@ func main() {
 	http.HandleFunc("/resumes", func(w http.ResponseWriter, req *http.Request) {
 		//q := events.goreq.URL.Query().Get("q")
 		_, _ = fmt.Fprintf(w, "个人信息：\n")
-		_, _ = fmt.Fprintf(w, "姓名：%s，\n性别：%s，\n年龄 %d!", conf.Resume.Name, conf.Resume.Sex, conf.Resume.Age) //这个写入到w的是输出到客户端的
+		_, _ = fmt.Fprintf(w, "姓名：%s，\n性别：%s，\n年龄 %d!", viper.GetString("resume.name"), conf.Resume.Sex, conf.Resume.Age) //这个写入到w的是输出到客户端的
 	})
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
