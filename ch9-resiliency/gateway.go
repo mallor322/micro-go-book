c2d8fad696a31263a5300a032869aba7f1a7128b
xuan <1533704796@qq.com>
2019-09-09 02:02:55 +0800

    tmp

--- ch9-resiliency/gateway.go
+++ ch9-resiliency/gateway.go
@@ -5,12 +5,9 @@ import (
 	"fmt"
 	"github.com/go-kit/kit/log"
 	"github.com/hashicorp/consul/api"
-	"math/rand"
 	"net/http"
-	"net/http/httputil"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 )
 
@@ -41,7 +38,7 @@ func main() {
 	}
 
 	//创建反向代理
-	proxy := NewReverseProxy(consulClient, logger)
+	proxy := NewHystrixHandler(consulClient, logger)
 
 	errc := make(chan error)
 	go func() {
@@ -58,47 +55,4 @@ func main() {
 
 	// 开始运行，等待结束
 	logger.Log("exit", <-errc)
-}
-
-// NewReverseProxy 创建反向代理处理方法
-func NewReverseProxy(client *api.Client, logger log.Logger) *httputil.ReverseProxy {
-
-	//创建Director
-	director := func(req *http.Request) {
-
-		//查询原始请求路径，如：/arithmetic/calculate/10/5
-		reqPath := req.URL.Path
-		if reqPath == "" {
-			return
-		}
-		//按照分隔符'/'对路径进行分解，获取服务名称serviceName
-		pathArray := strings.Split(reqPath, "/")
-		serviceName := pathArray[1]
-
-		//调用consul api查询serviceName的服务实例列表
-		result, _, err := client.Catalog().Service(serviceName, "", nil)
-		if err != nil {
-			logger.Log("ReverseProxy failed", "query service instace error", err.Error())
-			return
-		}
-
-		if len(result) == 0 {
-			logger.Log("ReverseProxy failed", "no such service instance", serviceName)
-			return
-		}
-
-		//重新组织请求路径，去掉服务名称部分
-		destPath := strings.Join(pathArray[2:], "/")
-
-		//随机选择一个服务实例
-		tgt := result[rand.Int()%len(result)]
-		logger.Log("service id", tgt.ServiceID)
-
-		//设置代理服务地址信息
-		req.URL.Scheme = "http"
-		req.URL.Host = fmt.Sprintf("%s:%d", tgt.ServiceAddress, tgt.ServicePort)
-		req.URL.Path = "/" + destPath
-	}
-	return &httputil.ReverseProxy{Director: director}
-
 }
