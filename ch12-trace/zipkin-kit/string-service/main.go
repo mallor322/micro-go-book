cd45f36869faa674e295a5903f1652bb018b2cd0
keets zhu <928420597@qq.com>
2019-10-22 23:54:27 +0800

    refactor kit trace

---
+++ ch12-trace/zipkin-kit/string-service/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/go-kit/kit/log"
+	kitzipkin "github.com/go-kit/kit/tracing/zipkin"
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	var (
+		consulHost  = flag.String("consul.host", "", "consul ip address")
+		consulPort  = flag.String("consul.port", "", "consul port")
+		serviceHost = flag.String("service.host", "", "service ip address")
+		servicePort = flag.String("service.port", "", "service port")
+		zipkinURL   = flag.String("zipkin.url", "http://106.15.233.99:9411/api/v2/spans", "Zipkin server url")
+	)
+
+	flag.Parse()
+
+	ctx := context.Background()
+	errChan := make(chan error)
+
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+		logger = log.With(logger, "caller", log.DefaultCaller)
+	}
+
+	var zipkinTracer *zipkin.Tracer
+	{
+		var (
+			err           error
+			hostPort      = *serviceHost + ":" + *servicePort
+			serviceName   = "string-service-service"
+			useNoopTracer = (*zipkinURL == "")
+			reporter      = zipkinhttp.NewReporter(*zipkinURL)
+		)
+		defer reporter.Close()
+		zEP, _ := zipkin.NewEndpoint(serviceName, hostPort)
+		zipkinTracer, err = zipkin.NewTracer(
+			reporter, zipkin.WithLocalEndpoint(zEP), zipkin.WithNoopTracer(useNoopTracer),
+		)
+		if err != nil {
+			logger.Log("err", err)
+			os.Exit(1)
+		}
+		if !useNoopTracer {
+			logger.Log("tracer", "Zipkin", "type", "Native", "URL", *zipkinURL)
+		}
+	}
+
+	var svc Service
+	svc = StringService{}
+
+	// add logging middleware to service
+	svc = LoggingMiddleware(logger)(svc)
+
+	endpoint := MakeStringEndpoint(svc)
+	endpoint = kitzipkin.TraceEndpoint(zipkinTracer, "calculate-endpoint")(endpoint)
+
+	//创建健康检查的Endpoint
+	healthEndpoint := MakeHealthCheckEndpoint(svc)
+	healthEndpoint = kitzipkin.TraceEndpoint(zipkinTracer, "health-endpoint")(healthEndpoint)
+
+	//把算术运算Endpoint和健康检查Endpoint封装至StringEndpoints
+	endpts := StringEndpoints{
+		StringEndpoint:      endpoint,
+		HealthCheckEndpoint: healthEndpoint,
+	}
+
+	//创建http.Handler
+	r := MakeHttpHandler(ctx, endpts, zipkinTracer, logger)
+
+	//创建注册对象
+	registar := Register(*consulHost, *consulPort, *serviceHost, *servicePort, logger)
+
+	go func() {
+		fmt.Println("Http Server start at port:" + *servicePort)
+		//启动前执行注册
+		registar.Register()
+		handler := r
+		errChan <- http.ListenAndServe(":"+*servicePort, handler)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	error := <-errChan
+	//服务退出取消注册
+	registar.Deregister()
+	fmt.Println(error)
+}
